b23fb58fa654b9f4acb98e298dbc811a8f5ab5d1
Jorge Romo <jromo@blacklabs.mx>
2018-04-08 21:43:01 -0600

    Se inicia proyecto api rest tercer strike

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict'
+//Se carga el archivo app.js
+
+
+const mongoose = require('mongoose');
+var app = require('./app');
+
+var port = process.env.port || 3000;
+app.set('view engine', 'pug');
+
+
+
+
+mongoose.connect('mongodb://localhost:27017/tercer_strike', (err,res) => {
+    if(err){
+        throw err;
+    }else{
+        console.log("Conexion a mongo exitosa");
+        app.listen(port, function () {
+            console.log(`API REST FAVORITOS FUNCIOadasNANDO en ${port}`);
+        });
+    }
+});
